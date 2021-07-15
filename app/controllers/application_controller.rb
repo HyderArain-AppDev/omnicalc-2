d7b6784c453bed4313d9171625b3e245afd70456
hyderarain <harain@chicagobooth.edu>
2021-07-15 01:59:30 +0000

    lat and lon is working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
-
+  def street_to_coords_form
+    render ({ :template => "coords_form.html.erb"})
+  end
+  def coords_result
+    @street_address = params.fetch("address")
+    @maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @street_address + "&key=AIzaSyD8RrOFB0dPsF-leqeFJdmX3yOvcQbfNyY"
+    @raw_json = open(@maps_url).read
+    @parsed_json = JSON.parse(@raw_json)
+    @results = @parsed_json.fetch("results")
+    @first_res = @results.at(0)
+    @geom = @first_res.fetch("geometry")
+    @location = @geom.fetch("location")
+    @lat = @location.fetch("lat")
+    @long = @location.fetch("lng")
+    
+    render ({ :template => "coords_result.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,9 @@ get("/wizard_multiply", {:controller => "multiply", :action => "result_multiply_
 
 get("/divide", {:controller => "divide", :action => "blank_divide_form"})
 get("wizard_divide", {:controller => "divide", :action => "result_divide_form"})
+
+get("/street_to_coords", {:controller => "application", :action => "street_to_coords_form"})
+get("/wizard_street_to_coords", {:controller => "application", :action => "coords_result"})
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
